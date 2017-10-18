b04267f4520975d5858b5f2d740f3b24f26f7a12
prasitthipongk <prasitthipongk@DESKTOP-VLE7AF6>
2017-10-18 16:09:47 +0700

    181060 แก้ไข

--- models/CasemedicineSearch.php
+++ models/CasemedicineSearch.php
@@ -56,20 +56,34 @@ class CasemedicineSearch extends Casemedicine
             // $query->where('0=1');
             return $dataProvider;
         }
-
+ $session = Yii::$app->session;
         // grid filtering conditions
-        $query->andFilterWhere([
-            'ID' => $this->ID,
-            'idcase' => $this->idcase,
-            'idmedicine' => $this->idmedicine,
-            'medicinepackage_id' => $this->medicinepackage_id,
-            'qty' => $this->qty,
-            'expired_date' => $this->expired_date,
-        ])->orderBy(['idcase' => SORT_DESC]);
-
-        $query->andFilterWhere(['like', 'properties', $this->properties])
-            ->andFilterWhere(['like', 'howto', $this->howto])
-            ->andFilterWhere(['like', 'note', $this->note]);
+        $query->joinWith('casepatient')->Where([
+            //'ID' => $this->ID,
+            //'appointment_time' => $this->appointment_time,
+            //'medical_certificate' => $this->medical_certificate,
+            //'todoctor' => $this->todoctor,
+            //'timestam' => $this->timestam,
+            //'user_id' => $this->user_id,
+            //'user_id2' => $this->user_id2,
+            'p_pid' => $session['pid'],
+            //'patient_p_sid' => $this->patient_p_sid,
+            //'casetype_idcasetype' => $this->casetype_idcasetype,
+            //'doctor_iddoctor' => $this->doctor_iddoctor,
+        ]);
+        
+//        andFilterWhere([
+//            'ID' => $this->ID,
+//            'idcase' => $this->idcase,
+//            'idmedicine' => $this->idmedicine,
+//            'medicinepackage_id' => $this->medicinepackage_id,
+//            'qty' => $this->qty,
+//            'expired_date' => $this->expired_date,
+//        ])->orderBy(['idcase' => SORT_DESC]);
+//
+//        $query->andFilterWhere(['like', 'properties', $this->properties])
+//            ->andFilterWhere(['like', 'howto', $this->howto])
+//            ->andFilterWhere(['like', 'note', $this->note]);
 
         return $dataProvider;
     }
