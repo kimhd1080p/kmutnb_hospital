f9663c5b1a74a62d60fd05a2f74dd7056eed3914
prasitthipongk <prasitthipongk@DESKTOP-VLE7AF6>
2017-11-02 19:36:51 +0700

    021160 ปริ้นสติกเกอร์ยา

--- views/medicine/view.php
+++ views/medicine/view.php
@@ -5,7 +5,7 @@ use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Medicine */
-
+$this->registerCss("#barcode {font-weight: normal; font-style: normal; line-height:normal; sans-serif; font-size: 12pt}");
 $this->title = 'ดูยา';
 $this->params['breadcrumbs'][] = ['label' => 'เครื่องมือ', 'url' => ['//tool/index']];
 $this->params['breadcrumbs'][] = ['label' => 'ยา', 'url' => ['medicine/index']];
@@ -20,7 +20,7 @@ $this->params['breadcrumbs'][] = $this->title;
  <div class="nav-tabs-custom">
             <!-- Tabs within a box -->
             <ul class="nav nav-tabs pull-right"> 
-              <li class="pull-left header"><i class="fa  fa-file-text"></i>ฟอร์ม</li>
+              <li class="pull-left header"><i class="fa  fa-file-text"></i>รายละเอียด</li>
             </ul>
           <!-- เนื้อหา -->
           <div class="box-body">
@@ -48,8 +48,49 @@ $this->params['breadcrumbs'][] = $this->title;
                   'medicinetype.medicinetype',
         ],
     ]) ?>
+ 
+</div>
+</div>
+ 
+ <div class="nav-tabs-custom">
+            <!-- Tabs within a box -->
+            <ul class="nav nav-tabs pull-right"> 
+              <li class="pull-left header"><i class="fa  fa-file-text"></i>บาร์โค๊ด</li>
+            </ul>
+          <!-- เนื้อหา -->
+          <div class="box-body">
+<div id="barcode" ><?= $model->idmedicine ?></div> 
 
 </div>
 </div>
+ 
 </div>
     </div>
+
+<?php
+$this->registerJs( <<< EOT_JS
+     
+     // using GET method
+        
+function get_object(id) {
+   var object = null;
+   if (document.layers) {
+    object = document.layers[id];
+   } else if (document.all) {
+    object = document.all[id];
+   } else if (document.getElementById) {
+    object = document.getElementById(id);
+   }
+   return object;
+  }
+get_object("barcode").innerHTML=DrawHTMLBarcode_UPCA(get_object("barcode").innerHTML,"yes","in",0,2,0.8,"bottom","center","","black","white");       
+
+
+ 
+   
+EOT_JS
+);
+$this->registerJsFile(
+    '@web/js/connectcode-javascript-upca.js'
+);
+?>
